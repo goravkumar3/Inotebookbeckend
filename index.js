90f2caee124fc7e26d16581f5bed8cda13b1487d
[gorav kumar] <[kg558390@gmail.com]>
2023-06-01 18:02:53 +0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const connectDb=require('./db');
 const express=require('express');
+const cors=require('cors')
 connectDb();
 const app=express();
+app.use(cors());
 const port=5000;
 app.use(express.json())
  app.use('/api/auth',require('./router/auth'))
